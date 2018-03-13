4f6a3f2fad910f62823ab5e2d766ce7206b967e3
Camelion <camelion.c@mail.ru>
2018-03-13 20:00:53 +0300

    get rid of unused files cleanup .gitignore include apache 2.0 according to license specification add missing gradle-wrapper.jar

--- src/test/java/com/github/mangelion/ClickHouseClientTest.java
+++ src/test/java/com/github/mangelion/ClickHouseClientTest.java
@@ -1,3 +1,19 @@
+/*
+ *    Copyright 2017-2018 Mangelion
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
 package com.github.mangelion;
 
 import com.github.mangelion.test.extensions.docker.DockerContainer;
@@ -87,7 +103,7 @@ final class ClickHouseClientTest {
     @DockerContainer(image = "yandex/clickhouse-client", net = "host", arguments = {
             "--multiquery",
             "--query=CREATE TABLE IF NOT EXISTS default.connection_test(date Date DEFAULT toDate(datetime), datetime DateTime DEFAULT now(), value UInt64) ENGINE = MergeTree(date, (date), 8192)"})
-    void sendSmallLongMultipleTimes_withCompression() throws SQLException {
+    void sendSmallLongMultipleTimes_withCompression() {
         client = client.compression(CompressionMethod.LZ4);
 
         Object[] data = new Object[]{1L};
